e5c51d45105da37a363d6345a1c743fcf8aa3ffa
MyeongchanJin <jmcchan@naver.com>
2020-03-11 22:22:39 +0900

    add gitignore

---
+++ src/main/java/jblog/controller/BlogController.java
@@ -0,0 +1,14 @@
+package jblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/blog")
+public class BlogController {
+	
+	@RequestMapping("/main")
+	public String blogMain() {
+		return "blog/main";
+	}
+}
